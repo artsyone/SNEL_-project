7e4cf073666174aecc3c29ff04fdeb9c7e8d25cb
artsyone <31486667+artsyone@users.noreply.github.com>
2019-02-01 10:31:28 -0500

    Add files via upload

---
+++ SNEL.py
@@ -0,0 +1,44 @@
+import random
+import math
+
+die_rolls = [1,2,3,4,5,6]
+outcomes = [11,9,7,8,14,11]
+
+
+#observed =
+
+def count_rolls():
+    rolls = 0 
+    for die in die_rolls:
+        rolls += 1
+    return rolls 
+
+def sum_of_all():
+    count = 0 
+    for o in outcomes:
+        count += o
+
+    return count
+
+
+def expected_count(rolls,count):
+
+    expected = (count/rolls)
+    
+    return expected
+
+
+
+def chi_square(expected):
+    V = 0 +2.2
+    for i in die_rolls:
+        V = ((i - expected)**2) /expected
+
+    print (V)
+
+
+count_rolls()
+sum_of_all()
+expected_count(count_rolls(), sum_of_all())
+chi_square(expected_count(count_rolls(), sum_of_all()))
+
