f82f33254f82b3fb0f92bcdf7b23645090a2ca26
artsyone <31486667+artsyone@users.noreply.github.com>
2019-02-06 10:31:05 -0500

    Add files via upload

--- SNEL.py
+++ SNEL.py
@@ -4,7 +4,6 @@ import math
 die_rolls = [1,2,3,4,5,6]
 outcomes = [11,9,7,8,14,11]
 
-#observed =
 
 def count_rolls():
     rolls = 0 
@@ -19,28 +18,64 @@ def sum_of_all():
 
     return count
 
-
 def expected_count(rolls,count):
 
     expected = (count/rolls)
     
     return expected
 
+def get_char_count():
+    count = [0] * 127
+    return counts[32:]
 
 
 def chi_square(expected):
-    V = 0 
-    for i in outcomes:
-        V = (i - expected) ** 2 / (expected)
+   print(sum([(i - expected) ** 2 / expected for i in outcomes]))
+   
+    
+def random_file_writer():
+    file_num = "0"
+
+    for n in range(num_files):
+        files =  open("file" + str(file_num) + '.txt', 'w' )
+
+        files.write(str(random.randrange(32,127)))
+        
+        file_num += str(1)
+
+#def get_file_content(file_name):
+        ''' helps with coops files'''
+    #with open(file_name,'r') as f :
+        #content = f.read()
+    #return content
+
+#lets do this
+num_files = 2
+threshold = 50
+
+#for n in range(num_files):
+    #if chi square > threshold:
+
+        #print(files,chi_square)
+
+
+    
+
+    
+
+
+
+
+
+
+
+
 
         
-        
-
-    print (V)
-
 
 count_rolls()
 sum_of_all()
 expected_count(count_rolls(), sum_of_all())
 chi_square(expected_count(count_rolls(), sum_of_all()))
+random_file_writer()
 
