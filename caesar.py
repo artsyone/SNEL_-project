42a46c2c28eaa2f87221b5dc767d3a64261b303f
artsyone <31486667+artsyone@users.noreply.github.com>
2019-02-24 23:22:52 -0500

    Add files via upload

--- caesar.py
+++ caesar.py
@@ -1,2 +1,22 @@
 def caesar_cipher():
-    pass
+
+    file = open("file6.txt", 'r')
+    contents = file.read()
+    new_char = ''
+    for char in contents:
+
+        
+
+        if ord(char) <= 65:
+            old_char = ord(char) + 61
+            new_char += chr(old_char)
+
+        else:
+            old_char = ord(char) - 34 
+            new_char += chr(old_char)
+
+            
+    return new_char
+
+
+print(caesar_cipher())
