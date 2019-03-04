bef9fa720c78b6edec10a6115f6a1e7d47f35287
artsyone <31486667+artsyone@users.noreply.github.com>
2019-03-04 10:06:45 -0500

    Add files via upload

--- vigenere.py
+++ vigenere.py
@@ -4,42 +4,20 @@ def vig_cipher():
     contents = file.read()
     answer = ''
     item = 0
-    num = key_word[item]
+    
     for char in contents:
-
+        num = key_word[item]
         new_char = ord(char) - ord(num)
         
+        if new_char < 32:
+            new_char + 95 
+
+        answer += chr(new_char)
+
+        item += 1 
         if item > 6:
-            item = 0 
-     
-        if new_char < 0:
-            if new_char + 95 > 126:
-                
-                #diff = 
-
-                final_char = ord(32 + new_char + diff)
-                answer += chr(final_char)
-            else:
-                new_char + 95 
-                final_char = ord( new_char)
-                answer += chr(final_char)
-                
-
-            else:
-                final_char = new_char + 95
-                answer += chr(final_char)
-                item += 1 
-            
-        else:
-            new_char
-            answer += chr(new_char)
-            item += 1 
-            
+            item = 0             
         
-
-            
-
-       
     return answer 
    
 
